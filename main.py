9b1f69240b9ca4c0174f4bee6ed8424d8fd756bc
Avishai Faitelson <avishai.faitelson@splitit.com>
2025-12-01 13:51:37 +0200

    search-agent-done

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
+from typing import List
+
+from pydantic import BaseModel
+from pydantic.fields import Field
 from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-import os
+
 load_dotenv()
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+	"""Schema for Source used by the agent"""
+	url:str = Field(description="URL of the source")
+
+class AgentResponse(BaseModel):
+	"""Schema for AgentResponse used by the agent"""
+	answer:str = Field(description="The agent's answer to the question")
+	sources: List[Source] = Field(default_factory=list, description="List of sources to use")
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools, response_format= AgentResponse)
 def main():
-    information = """
-		    Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
+    print("Hello from langchain-course!")
+    result = agent.invoke({"messages":[HumanMessage(content="search for 3 jobs posting for an ai engineer using langchain in Tel Aviv area on linkedin and list their details")]})
+    print(result)
 
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; his Canadian citizenship is congenital, his mother having been born there. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
-    """
-    
-    summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short Summary
-    2. two interesting facts about them
-    """
-    os.environ()
-    summary_prompt_template = PromptTemplate(
-	    input_variables=["information"], template=summary_template
-    )
-    
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
-    chain = summary_prompt_template | llm
-    response = chain.invoke(input={"information":information})
-    print(response.content)
 
 if __name__ == "__main__":
     main()
